2e224506076a7e04170e4c3e23a6bdbb7dc153a5
lheleg <lheleg1@etf.unsa.ba>
2024-01-21 23:20:16 +0100

    small changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("wt24", "root", "", {
+const sequelize = new Sequelize("wt24", "root", "password", {
   host: "127.0.0.1",
   dialect: "mysql",
   logging: false,
